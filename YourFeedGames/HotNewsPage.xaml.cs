388caff76d664fa88c9cd81327e23d69c59d5d69
Andrei B. Pacheco <andrei_pacheco@yahoo.com.br>
2025-05-12 11:20:56 -0300

    Adicionado nova HotNews

--- YourFeedGames/HotNewsPage.xaml.cs
+++ YourFeedGames/HotNewsPage.xaml.cs
@@ -14,5 +14,11 @@ namespace YourFeedGames
             var url = "https://www.rockstargames.com/";
             await Launcher.OpenAsync(new Uri(url));
         }
+
+        private async void On2KLinkClicked(object sender, EventArgs e)
+        {
+            var url = "https://mafia.2k.com/the-old-country/";
+            await Launcher.OpenAsync(new Uri(url));
+        }
     }
 }
